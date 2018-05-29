6b9ec76c1565fd1982040e987944b14db24c42ce
stevenshuang <stevenshuang521@gmail.com>
2018-05-29 18:32:43 +0800

    random ip choice API

--- pp/api.py
+++ pp/api.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-from flask import Flask
+from flask import Flask, g
 from RedisClient import RedisClient
 
 
@@ -15,18 +15,22 @@ def get_conn():
         g.redis = RedisClient()
     return g.redis
 
+
 @app.route('/')
 def index():
     return '<h1> Proxy Pool System</h1>'
 
 
 @app.route('/count')
-def get_count():
+def get_counts():
     conn = get_conn()
     return str(conn.count())
+
+@app.route('/random')
+def get_random():
+    return get_conn().random()
 
 
 if __name__ == '__main__':
     app.run()
-
 
